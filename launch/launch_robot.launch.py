aff1ba50d169c97867e2bc651f1d724aed66834a
asak <abdoorahmansk2004@gmail.com>
2024-04-27 19:39:11 +0300

    mod2

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler, TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.event_handlers import OnProcessStart
@@ -24,21 +24,26 @@ def generate_launch_description():
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'false', "use_ros2_control":'false'}.items()
     )
-    
+
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-    controller_params = os.path.join(
-        get_package_share_directory('my_bot'),
-        'config',
-        'my_controllers.yaml'
-        )
+    controller_params_file = os.path.join(
+                    get_package_share_directory(package_name),'config','my_controllers.yaml')
+    
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
     diff_drive_spawner = Node(
-            package='controller_manager',
-            executable='spawner',
-            name='controller_spawner',
-            output='screen',
-            arguments=['diff_cont', 'joint_broad'],
-        )
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
     delayed_diff_drive_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
             target_action=controller_manager,
@@ -46,23 +51,27 @@ def generate_launch_description():
         )
     )
 
-    controller_manager = Node(
-            package='controller_manager',
-            executable='ros2_control_node',
-            parameters=[{'robot_description': robot_description}, 
-                        controller_params]
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
         )
-    delay_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    )
 
-    
-
-
-    # Launch them all!
+    # Launc them all!
     return LaunchDescription([
         rsp,
-        delay_controller_manager,
-        delayed_diff_drive_spawner
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner,
     ])
+
 
 
 
